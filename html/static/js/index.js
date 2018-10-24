0e2107fee06439682d78ee548f8d3ad4316cd3bc
Rishitha Minol <rishithaminol@gmail.com>
2018-10-24 23:30:37 +0530

    added custom message popup

--- html/static/js/index.js
+++ html/static/js/index.js
@@ -7,4 +7,10 @@ $(function() {
   socket.on('browser reload', function (data) {
     document.location.reload(true);
   });
+
+  $(".close.icon", "#notification").click(function(){
+    $("#notification").fadeOut("slow");
+  });
+
+  $("#notification").delay(1000).slideDown("slow").delay(3000).slideUp("slow");
 });
